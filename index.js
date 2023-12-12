36fa060b3375633687a7ed5eafa673f2fd5e8caa
ramzimammadli <remzimemmedli95@gmail.com>
2023-12-12 09:50:22 +0400

    sort method done

--- index.js
+++ index.js
@@ -20,24 +20,26 @@ function getPro () {
 }
 getPro()
 
-function getbyname () {
-    abcd.innerHTML = ``
-    abc.style.display='none'
-    abcd.style.display='block'
+////////////////////////////////////////////////search By name
 
-    axios.get(`https://655c81de25b76d9884fd6913.mockapi.io/products?title=${inpp.value}`)
-    .then(res => {
-        db = res.data
-        db.map(item => {
-            const div = document.createElement('div')
-            div.innerHTML = `
-            <p>${item.title}</p>`
-            abcd.append(div)
-        })
+// function getbyname () {
+//     abcd.innerHTML = ``
+//     abc.style.display='none'
+//     abcd.style.display='block'
 
-    })
-}
-btn.addEventListener('click', getbyname)
+//     axios.get(`https://655c81de25b76d9884fd6913.mockapi.io/products?title=${inpp.value}`)
+//     .then(res => {
+//         db = res.data
+//         db.map(item => {
+//             const div = document.createElement('div')
+//             div.innerHTML = `
+//             <p>${item.title}</p>`
+//             abcd.append(div)
+//         })
+
+//     })
+// }
+// btn.addEventListener('click', getbyname)
 
 // function getbyname () {
 //     abcd.innerHTML = ``
@@ -59,3 +61,52 @@ btn.addEventListener('click', getbyname)
 //     })
 // }
 // btn.addEventListener('click', getbyname)
+
+////////////////////////////////////////////sort elifbaya gore
+
+// function getbyname () {
+//     abcd.innerHTML = ``
+//     abc.style.display='none'
+//     abcd.style.display='block'
+
+//     axios.get('https://655c81de25b76d9884fd6913.mockapi.io/products')
+//     .then(res => {
+//         db = res.data
+//         let sortedData = db.sort((a, b) => a.title.localeCompare(b.title))      
+//         sortedData.map(item => {
+//             const div = document.createElement('div')
+//             div.innerHTML = `
+//             <p>${item.title}</p>`
+//             abcd.append(div)
+//         })
+
+//     })
+// }
+// btn.addEventListener('click', getbyname)
+
+
+
+
+
+
+/////////////////////////////////sort qiymete gore
+
+function getbyname () {
+    abcd.innerHTML = ``
+    abc.style.display='none'
+    abcd.style.display='block'
+
+    axios.get('https://655c81de25b76d9884fd6913.mockapi.io/products')
+    .then(res => {
+        db = res.data
+        let sortedData = db.sort((a, b) => a.price - b.price)      
+        sortedData.map(item => {
+            const div = document.createElement('div')
+            div.innerHTML = `
+            <p>${item.title}</p>`
+            abcd.append(div)
+        })
+
+    })
+}
+btn.addEventListener('click', getbyname)
